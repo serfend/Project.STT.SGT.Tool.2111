fee25ba4ac3c8269b2975bf7027fb722dfb9c118
serfend <serfend@foxmail.com>
2021-11-09 21:20:34 +0800

    feat[log]frame-inited

--- Services/Log/TooltipLabelLogger.cs
+++ Services/Log/TooltipLabelLogger.cs
@@ -6,25 +6,22 @@ using System.Windows.Forms;
 
 namespace Project.STT.SGT.Tool._2111.Services.Log
 {
-    public class TooltipLabelLogger : NLog.Logger
+    public class TooltipLabelLogger
     {
         public readonly static Action<Logger, string> InfoLog = (l, m) => l.Log<string>(LogLevel.Info, m);
+        private readonly Logger logger;
         public void ActionWithLabel(Action<Logger, string> action, string message)
         {
             label.Text = message;
-            action.Invoke(this, message);
+            action.Invoke(logger, message);
         }
         private readonly ToolStripStatusLabel label;
 
         public TooltipLabelLogger(ToolStripStatusLabel label)
         {
             this.label = label;
-            this.LoggerReconfigured += TooltipLabelLogger_LoggerReconfigured;
+            logger = LogManager.GetCurrentClassLogger();
         }
 
-        private void TooltipLabelLogger_LoggerReconfigured(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
