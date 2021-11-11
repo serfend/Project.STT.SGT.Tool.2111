b607a4225e563c5fce842ec99c2a3f929e76235a
serfend <serfend@foxmail.com>
2021-11-11 10:41:22 +0800

    fix[vosk]remove un-recognize part

--- Services/STT/VoskResultEventArgs.cs
+++ Services/STT/VoskResultEventArgs.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace Project.STT.SGT.Tool._2111.Services.STT
 {
@@ -16,20 +17,23 @@ namespace Project.STT.SGT.Tool._2111.Services.STT
         public VoskResultEventArgs(string message, bool isPartial)
         {
             Data = JsonSerializer.Deserialize<VoskPartialResult>(message);
+            Data.Partial = Regex.Replace(Data.Partial, @"\[.*\]", string.Empty);
             this.IsPartial = isPartial;
         }
     }
     public class VoskFinnalResultEventArgs : EventArgs, IVoskSingleWordResult
     {
-        public VoskFinnalResultEventArgs(string message,bool isCompleted)
+        public VoskFinnalResultEventArgs(string message, bool isCompleted)
         {
             this.IsCompleted = isCompleted;
             Data = JsonSerializer.Deserialize<VoskSingleLineResult>(message);
-            Content = Data.Text;
+            Content = Regex.Replace(Data.Text, @"\[.*\]", string.Empty);
             Caculate();
         }
-        private void Caculate() {
-            foreach(var r in Data.Result)
+        private void Caculate()
+        {
+            if (Data.Result == null) return;
+            foreach (var r in Data.Result)
             {
                 TotalWords++;
                 Confidence += r.Confidence;
@@ -43,10 +47,10 @@ namespace Project.STT.SGT.Tool._2111.Services.STT
         /// </summary>
         public int TotalWords { get; set; }
         public VoskSingleLineResult Data { get; set; }
-        public string Content { get ;set; }
-        public double Confidence { get ;set; }
+        public string Content { get; set; }
+        public double Confidence { get; set; }
         public double Start { get; set; }
-        public double End { get ;set; }
+        public double End { get; set; }
         /// <summary>
         /// 是否已完成
         /// </summary>
